0f5947c1f295f23e51ecc1d12c05f970894e0343
nishant-ranjan28 <nishantranjan78@gmail.com>
2022-08-10 12:39:06 +0530

    copies base class

---
+++ src/Base2.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Base2 {
+	
+	public static void main(String[] args) throws MalformedURLException {
+		
+		
+		File appDir = new File("src");
+		File app = new File(appDir, "ApiDemos-debug.apk");
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2XL");
+		
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		
+		//UI Automator  -> Android apps 
+		//UIautomator2
+		
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		
+	}
+
+}
